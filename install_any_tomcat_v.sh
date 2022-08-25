c5995bc06f182a8b9fea8865dc539e54f1d1fe1a
Azad Gunjal <110171614+Ciphe-R@users.noreply.github.com>
2022-08-25 16:06:23 +0530

    Update install_any_tomcat_v.sh

--- install_any_tomcat_v.sh
+++ install_any_tomcat_v.sh
@@ -1,7 +1,7 @@
 #./usr/bin/bash
 
 #tom_version="10.1.0-M17"
-tom_version=$1			# to take input from terminal
+read -p "Version to Download? " tom_version	            # to take input from terminal
 tom_m_ver=$(echo $tom_version | cut -d "." -f 1)
 url="https://dlcdn.apache.org/tomcat/tomcat-${tom_m_ver}/v${tom_version}/bin/apache-tomcat-${tom_version}.tar.gz"
 
